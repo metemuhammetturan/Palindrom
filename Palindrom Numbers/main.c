8d29de6f5553d6488f4440d67c1deebb2f02317c
Mete Muhammet Turan <mete3381@gmail.com>
2023-12-22 15:20:24 +0300

    Palindrom

--- Palindrom Numbers/main.c
+++ Palindrom Numbers/main.c
@@ -5,10 +5,33 @@
 //  Created by Mete Turan on 22.12.2023.
 //
 
-#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include <stdio.h>
+#include <stdbool.h> // bool veri tipi için stdbool.h kütüphanesi eklendi
+
+bool isPalindrome(int x) {
+    if (x < 0) {
+        return false;
+    }
+
+    int reversed = 0;
+    int temp = x;
+
+    while (temp != 0) {
+        int digit = temp % 10;
+        reversed = reversed * 10 + digit;
+        temp /= 10;
+    }
+
+    return (reversed == x);
+}
+
+int main() {
+    int number = 12321;
+    if (isPalindrome(number)) {
+        printf("%d is a palindrome number.\n", number);
+    } else {
+        printf("%d is not a palindrome number.\n", number);
+    }
     return 0;
 }
